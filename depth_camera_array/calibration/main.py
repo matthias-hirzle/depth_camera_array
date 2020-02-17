b1ee5c9a6e6e1ffcb65ec133ad0c056c4cd1b96a
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-17 15:27:34 +0100

    defined some arguments for calibration

--- depth_camera_array/calibration/main.py
+++ depth_camera_array/calibration/main.py
@@ -3,10 +3,13 @@ import argparse
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cameras', action=list)  # TODO: information to identify cameras and catch them
+    parser.add_argument('--output', help='Path to output file')
     return parser.parse_args()
 
 
 def main():
+    """Creates a camera setup file containing camera ids and extrinsics"""
     args = parse_args()
 
 
