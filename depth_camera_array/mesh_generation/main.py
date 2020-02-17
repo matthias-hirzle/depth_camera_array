3115398405cfdf37881e05dfb5dee021474e99b7
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-17 15:30:16 +0100

    defined some parameters for mesh generation.

--- depth_camera_array/mesh_generation/main.py
+++ depth_camera_array/mesh_generation/main.py
@@ -2,7 +2,9 @@ import argparse
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser('Performes a mesh generation to the given coordinates')
+    parser.add_argument('--coordinates', type=str, help='Path to file containing coordinates')  # TODO how does it look like
+    parser.add_argument('--output', type=str, help='Path to output file')
     return parser.parse_args()
 
 
