38946700cd380f79a8e03735df9f13d394deaa95
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-18 09:55:57 +0100

    function to determine connected cameras.

--- depth_camera_array/camera.py
+++ depth_camera_array/camera.py
@@ -1,8 +1,28 @@
 from pyrealsense2 import pyrealsense2 as rs
 
+
+def initialize_connected_cameras() -> list:
+    """
+    Enumerate the connected Intel RealSense devices
+    Parameters:
+    -----------
+    context 	   : rs.context()
+                     The context created for using the realsense library
+    Return:
+    -----------
+    connect_device : array
+                     Array of enumerated devices which are connected to the PC
+    """
+    context = rs.context()
+    devices = []
+    for device in context.devices:
+        if device.get_info(rs.camera_info.name).lower() != 'platform camera':
+            devices.append(device.get_info(rs.camera_info.serial_number))
+    return devices
+
+
 class DepthCamera:
     def __init__(self, device_id: str):
         self.pipeline = rs.pipeline()
         self.config = rs.config
         pass
-
