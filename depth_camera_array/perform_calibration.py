c10fadd34f82a26214bf7b1ca5cd675d1b072250
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-18 12:14:54 +0100

    testcase for camera access.

--- depth_camera_array/perform_calibration.py
+++ depth_camera_array/perform_calibration.py
@@ -1,4 +1,6 @@
 import argparse
+import pyrealsense2 as rs
+from depth_camera_array.camera import initialize_connected_cameras
 
 
 def parse_args() -> argparse.Namespace:
@@ -9,7 +11,22 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     """Creates a camera setup file containing camera ids and extrinsic information"""
-    args = parse_args()
+    # args = parse_args()
+    #cameras = initialize_connected_cameras()
+    #print(len(cameras))
+    #cameras[0].poll_frames()
+    pipeline = rs.pipeline()
+    pipeline.start()
+
+    try:
+        while True:
+            # Create a pipeline object. This object configures the streaming camera and owns it's handle
+            frames = pipeline.wait_for_frames()
+            depth = frames.get_depth_frame()
+            if not depth: continue
+
+    finally:
+        pipeline.stop()
 
 
 if __name__ == '__main__':
