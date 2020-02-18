fb5a5312dd2727adade5e31abccc78b2c9c1a136
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-18 17:04:35 +0100

    adjusted functionality

--- depth_camera_array/perform_calibration.py
+++ depth_camera_array/perform_calibration.py
@@ -1,5 +1,11 @@
 import argparse
+
+import numpy as np
 import pyrealsense2 as rs
+from cv2 import cv2
+
+from depth_camera_array.calibrator import find_qr
+# from depth_camera_array.camera import initialize_connected_cameras
 from depth_camera_array.camera import initialize_connected_cameras
 
 
@@ -9,24 +15,56 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main():
-    """Creates a camera setup file containing camera ids and extrinsic information"""
-    # args = parse_args()
-    #cameras = initialize_connected_cameras()
-    #print(len(cameras))
-    #cameras[0].poll_frames()
+def dump_scene():
+    try:
+        cameras = initialize_connected_cameras()
+        for camera in cameras:
+            for k, v in camera.poll_frames().items():
+                v.dump(f'/home/matze/projects/depth_camera_array/data/{camera._device_id}_{k}')
+    except RuntimeError as error:
+        print(error)
+    finally:
+        camera.close()
+
+
+
+#cv2.imwrite(f'/home/matze/projects/depth_camera_array/data/array_{camera._device_id}.png', frames['color'])
+
+
+def check_single_rgb():
     pipeline = rs.pipeline()
     pipeline.start()
 
-    try:
-        while True:
-            # Create a pipeline object. This object configures the streaming camera and owns it's handle
-            frames = pipeline.wait_for_frames()
-            depth = frames.get_depth_frame()
-            if not depth: continue
+    # Create a pipeline object. This object configures the streaming camera and owns it's handle
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+    color_image = np.asanyarray(color_frame.get_data())
+    # rgb = np.random.randint(255, size=(900, 800, 3), dtype=np.uint8)
+    cv2.imwrite('/home/matze/projects/depth_camera_array/data/single.png', color_image)
+    pipeline.stop()
 
-    finally:
-        pipeline.stop()
+
+def main():
+    """Creates a camera setup file containing camera ids and extrinsic information"""
+    # args = parse_args()
+    # cameras = initialize_connected_cameras()
+    # print(len(cameras))
+    # cameras[0].poll_frames()
+    # check_single_rgb()
+    dump_scene()
+    # find_qr('/home/matze/projects/depth_camera_array/data/single.png')
+
+    # 1. Identify cameras
+    # 2. Take frames rgb and depth for each camera
+    # 3. qr detection through calibrator
+    # 4. intersection of detected points through qr id
+    # 5. generate extrinsics
+    # 6. take frames from bottom
+    # 7. determine direction of z-axis
+    # 8. adjust all extrinsics
+    # 9. dump extrinsics and device ids for next step.
+
+
 
 
 if __name__ == '__main__':
