5655084443c118667b93e89e0d0a2832a837c4c0
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-19 14:13:42 +0100

    renamed camera class

--- depth_camera_array/camera.py
+++ depth_camera_array/camera.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Any
 
 import numpy as np
 from pyrealsense2 import pyrealsense2 as rs
 
 
-class DepthCamera:
+class Camera:
     def __init__(self, device_id: str, context: rs.context):
         resolution_width = 1280
         resolution_height = 720
@@ -24,17 +24,35 @@ class DepthCamera:
 
         # self._profile = self._pipeline.start(self._config)
 
-    def poll_frames(self):
+    def poll_frames(self) -> rs.composite_frame:
         # streams = self._profile.get_streams()
         # color = rs.pipeline_profile.get_stream(self._profile, rs.stream.color)
         frames = self._pipeline.wait_for_frames()
-        return {
-            'color': np.asanyarray(frames.get_color_frame().get_data()),
-            'depth': np.asanyarray(frames.get_depth_frame().get_data())
-        }
+        # return {
+        #     'color': np.asanyarray(frames.get_color_frame().get_data()),
+        #     'depth': np.asanyarray(frames.get_depth_frame().get_data())
+        # }
+        return frames
 
     def close(self):
         self._pipeline.stop()
+
+    @staticmethod
+    def image_points_to_object_points(image_points: np.array, frames: rs.composite_frame) -> Any:
+        """Calculates the object points for given image points"""
+        # depth = frames.get_depth_frame()
+        # depth_profile = depth.get_profile()
+        #
+        # color: rs.video_frame = frames.get_color_frame()
+        # color_profile: rs.stream_profile = color.get_profile()
+        #
+        # extrinsics: rs.extrinsics = color_profile.get_extrinsics_to(depth_profile)
+
+        pass
+
+    @staticmethod
+    def extract_color_image(frames: rs.composite_frame) -> np.ndarray:
+        return np.asanyarray(frames.get_color_frame().get_data())
 
 
 def _find_connected_devices(context):
@@ -45,7 +63,7 @@ def _find_connected_devices(context):
     return devices
 
 
-def initialize_connected_cameras() -> List[DepthCamera]:
+def initialize_connected_cameras() -> List[Camera]:
     """
     Enumerate the connected Intel RealSense devices
     Parameters:
@@ -61,5 +79,5 @@ def initialize_connected_cameras() -> List[DepthCamera]:
     context = rs.context()
     device_ids = _find_connected_devices(context)
 
-    devices = [DepthCamera(device_id, context) for device_id in device_ids]
+    devices = [Camera(device_id, context) for device_id in device_ids]
     return devices
