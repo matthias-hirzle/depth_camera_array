c661a7f63ad5cfcfe20324d7983aa0b5bcc72089
Joshua Reimer <rejo1020@hs-karlsruhe.de>
2020-02-19 17:28:45 +0100

    used arguments to detect file path for saving JSON files, adjusted implementation to camera API, adjusted data types like nparray <> list

--- depth_camera_array/perform_aruco_detection.py
+++ depth_camera_array/perform_aruco_detection.py
@@ -1,9 +1,11 @@
 import argparse
+import os
+
 from cv2 import aruco
 import numpy as np
 import json
 
-from depth_camera_array.camera import initialize_connected_cameras, extract_color_image
+from depth_camera_array.camera import initialize_connected_cameras, extract_color_image, image_points_to_object_points
 from typing import List, Tuple
 
 from depth_camera_array.utilities import get_or_create_data_path
@@ -17,14 +19,14 @@ def parse_args() -> argparse.Namespace:
 
 
 def main():
-    # args = parse_args()
+    args = parse_args()
     cameras = initialize_connected_cameras()
     for cam in cameras:
         frames = cam.poll_frames()
         all_2d_centers_of_arucos, all_detected_aruco_ids = read_aruco_codes_from_frame(frames)
-        tree_dimensional_points = cam.image_points_to_object_points(all_2d_centers_of_arucos, frames)
+        tree_dimensional_points = image_points_to_object_points(all_2d_centers_of_arucos, frames)
         assert len(tree_dimensional_points) == len(all_detected_aruco_ids)
-        dump_arcuro_data(cam._device_id, all_detected_aruco_ids, tree_dimensional_points)
+        dump_arcuro_data(cam._device_id, all_detected_aruco_ids, tree_dimensional_points, args.base_path)
 
 
 def read_aruco_codes_from_frame(frames):
@@ -36,10 +38,10 @@ def read_aruco_codes_from_frame(frames):
         act_coord = detected_coords[index]
         center_of_actual_code = calc_center_coordinates(act_coord)
         all_2d_centers.append(center_of_actual_code)
-    return all_2d_centers, ids
+    return all_2d_centers, ids.tolist()
 
 
-def calc_center_coordinates(corners) -> np.array(float):
+def calc_center_coordinates(corners) -> List[float]:
     coordinates = corners[0]
     x_sum = 0
     y_sum = 0
@@ -47,10 +49,10 @@ def calc_center_coordinates(corners) -> np.array(float):
     for coord in coordinates:
         x_sum += coord[0]
         y_sum += coord[1]
-    return np.array([x_sum / 4, y_sum / 4])
+    return [x_sum / 4, y_sum / 4]
 
 # write data to json file
-def dump_arcuro_data(camera_id: str, code_id_array: List[str], coordinate_array: List[Tuple[int, int, int]]):
+def dump_arcuro_data(camera_id: str, code_id_array: List[str], coordinate_array: List[Tuple[float, float, float]], base_path: str):
 
     new_dict = {}
     new_dict.update(camera_id=camera_id)
@@ -58,7 +60,7 @@ def dump_arcuro_data(camera_id: str, code_id_array: List[str], coordinate_array:
     new_dict.update(centers=coordinate_array)
 
     # write to a file
-    with open('{}_reference_points.json'.format(camera_id), 'w') as json_file:
+    with open(os.path.join(base_path, f'{camera_id}_reference_points.json'), 'w')as json_file:
         json.dump(new_dict, json_file)
 
 
