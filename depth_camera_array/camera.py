03478c437413703c7740314cf1618167b5b17645
matthias-hirzle <hima1021@hs-karlsruhe.de>
2020-02-19 16:30:56 +0100

    implemented function to map from colorpixel to real world coordinates

--- depth_camera_array/camera.py
+++ depth_camera_array/camera.py
@@ -1,4 +1,3 @@
-import json
 from typing import List, Any
 
 import numpy as np
@@ -39,24 +38,35 @@ class Camera:
     def close(self):
         self._pipeline.stop()
 
-    def image_points_to_object_points(self, image_points: np.array, frames: rs.composite_frame) -> Any:
-        """Calculates the object points for given image points"""
-        color: rs.video_frame = frames.get_color_frame()
-        depth = frames.get_depth_frame()
 
+def image_points_to_object_points(color_pixels: np.array, frames: rs.composite_frame) -> Any:
+    """Calculates the object points for given image points"""
+    color: rs.video_frame = frames.get_color_frame()
+    depth: rs.depth_frame = frames.get_depth_frame()
 
-        color_profile: rs.stream_profile = color.get_profile()
-        depth_profile = depth.get_profile()
+    color_profile: rs.stream_profile = color.get_profile()
+    depth_profile = depth.get_profile()
 
-        # depth_intrinsics: rs.intrinsics =
+    color_intrinsics: rs.intrinsics = color_profile.as_video_stream_profile().get_intrinsics()
+    depth_intrinsics: rs.intrinsics = depth_profile.as_video_stream_profile().get_intrinsics()
 
+    color_to_depth_extrinsics: rs.extrinsics = color_profile.get_extrinsics_to(depth_profile)
+    depth_to_color_extrinsics: rs.extrinsics = depth_profile.get_extrinsics_to(color_profile)
 
-        color_to_depth_extrinsics: rs.extrinsics = color_profile.get_extrinsics_to(depth_profile)
-        depth_to_color_extrinsics: rs.extrinsics = depth_profile.get_extrinsics_to(color_profile)
-
-        rs.rs2_project_color_pixel_to_depth_pixel()
-        rs.rs2_deproject_pixel_to_point()
-        return [[0,0,0]]*len(image_points)
+    depth_pixels = [
+        rs.rs2_project_color_pixel_to_depth_pixel(
+            data=depth.get_data(),
+            depth_scale=1,
+            depth_min=0.1,
+            depth_max=10,
+            depth_intrin=depth_intrinsics,
+            color_intrin=color_intrinsics,
+            depth_to_color=depth_to_color_extrinsics,
+            color_to_depth=color_to_depth_extrinsics,
+            from_pixel=color_pixel
+        ) for color_pixel in color_pixels
+    ]
+    return [rs.rs2_deproject_pixel_to_point(depth_intrinsics, pixel) for pixel in depth_pixels]
 
 
 def extract_color_image(frames: rs.composite_frame) -> np.ndarray:
