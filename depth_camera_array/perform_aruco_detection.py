899a45a6f69c6a489db23f217b3d839d52a743c0
Joshua Reimer <rejo1020@hs-karlsruhe.de>
2020-02-19 15:17:48 +0100

    added implementation for reading aruco codes

--- depth_camera_array/perform_aruco_detection.py
+++ depth_camera_array/perform_aruco_detection.py
@@ -1,4 +1,7 @@
 import argparse
+from depth_camera_array import camera
+from cv2 import cv2
+from cv2 import aruco
 
 # from depth_camera_array.camera import initialize_connected_cameras
 from typing import List, Tuple
@@ -14,7 +17,33 @@ def parse_args() -> argparse.Namespace:
 
 
 def main():
-    args = parse_args()
+    # args = parse_args()
+    cameras = camera.initialize_connected_cameras()
+    # rgb_image = cv2.imread("817612070307_color.png") #for testing reasons without having a cam connected
+    for cam in cameras:
+        frames = cam.poll_frames()
+        rgb_image = cam.extract_color_image(frames)
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
+        detected_coords, ids, _ = aruco.detectMarkers(rgb_image, aruco_dict)
+        all_3d_points = []
+        for index, cam_id in enumerate(ids):  # for each found aruco-code
+            act_coord = detected_coords[index]
+            centers = calc_center_coordinates(act_coord)
+            tree_dimensional_points = cam.image_points_to_object_points(centers, frames)
+            all_3d_points.append(tree_dimensional_points)
+            print(centers)
+        assert len(all_3d_points) == len(ids)
+        dump_arcuro_data(cam._device_id, ids, all_3d_points)
+
+def calc_center_coordinates(corners) -> Tuple[int, int]:
+    coordinates = corners[0]
+    x_sum = 0
+    y_sum = 0
+    assert len(coordinates) == 4  # should find four corner coordinates
+    for coord in coordinates:
+        x_sum += coord[0]
+        y_sum += coord[1]
+    return [x_sum / 4, y_sum / 4]
 
 def dump_arcuro_data(camera_id: str, code_id_array: List[str], coordinate_array: List[Tuple[int, int, int]]):
     pass
